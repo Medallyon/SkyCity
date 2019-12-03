b59be2e1f85efc3252d820ff687859fa9d6dd983
Tilman Raendchen <TRaendchen@outlook.com>
2019-12-03 13:25:43 +0000

    Create a basic working HUD

---
+++ Assets/Scripts/ObjectiveHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectiveHandler : MonoBehaviour
+{
+    public GameObject CityGenerator;
+
+    [Header("UI")]
+    public Text EnemiesLeftText;
+
+    private int enemiesToDefeat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.enemiesToDefeat = this.CityGenerator.GetComponent<CityGenerator>().EnemiesToSpawn;
+        this.EnemiesLeftText.text = this.enemiesToDefeat.ToString();
+    }
+
+    private List<GameObject> defeated;
+    public void defeatEnemy(GameObject enemy)
+    {
+        this.defeated.Add(enemy);
+        this.UpdateEnemyText();
+    }
+
+    void UpdateEnemyText()
+    {
+        this.EnemiesLeftText.text = (this.enemiesToDefeat - this.defeated.Count).ToString();
+    }
+}
--- Assets/Scripts/Player/ScaledMovement.cs
+++ Assets/Scripts/Player/ScaledMovement.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScaledMovement : MonoBehaviour
 {
     public float Speed = 10f;
     public float TopSpeed = 80f;
+
+    [Header("UI")]
+    public Slider Accelerometer;
+    public Image VelocityFill;
 
     private float acceleration = 0f;
     private float Acceleration
@@ -18,6 +23,7 @@ public class ScaledMovement : MonoBehaviour
         {
             // Limit acceleration with min and max bounds based on public TopSpeed
             this.acceleration = Mathf.Max(-this.TopSpeed / 2, Mathf.Min(value, this.TopSpeed));
+            this.Accelerometer.value = MapRangeClamped(0, 1, -this.TopSpeed / 2, this.TopSpeed, this.Acceleration);
         }
     }
 
@@ -27,6 +33,12 @@ public class ScaledMovement : MonoBehaviour
     void Start()
     {
         this.rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // TODO: Reference slider fill image and sync it with the actual current velocity
+        //this.
     }
 
     void FixedUpdate()
@@ -40,13 +52,20 @@ public class ScaledMovement : MonoBehaviour
 
         this.rb.velocity = Vector3.Lerp(this.rb.velocity, this.transform.forward * this.Acceleration, Time.deltaTime / 2);
 
-        Debug.Log($"{this.Acceleration}, {this.rb.velocity}");
-
         // Rotation Lerp
         this.eulerRotation.y = Mathf.Lerp(this.eulerRotation.y, Input.GetAxis("Rotation") * 100f, Time.deltaTime);
 
         // Rotate Ship
         var deltaRotation = Quaternion.Euler(this.eulerRotation * Time.deltaTime);
         this.rb.MoveRotation(this.rb.rotation * deltaRotation);
+    }
+
+    static float MapRangeClamped(float from1, float to1, float from2, float to2, float value)
+    {
+        if (value <= from2)
+            return from1;
+        if (value >= to2)
+            return to1;
+        return (to1 - from1) * ((value - from2) / (to2 - from2)) + from1;
     }
 }
